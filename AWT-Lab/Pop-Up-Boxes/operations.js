81f6eb572c0b894e398ea1ae384c73e4dd2820c5
Adelicia <a.seq2804@gmail.com>
2023-04-06 10:38:11 +0530

    updated q6

--- AWT-Lab/Pop-Up-Boxes/operations.js
+++ AWT-Lab/Pop-Up-Boxes/operations.js
@@ -1,46 +1,76 @@
 const displayDate = () => {
-    date = new Date();
-    day = date.getDate();
-    month = date.getMonth() + 1;
-    year = date.getFullYear();
-    window.alert(`Today's date is ${day}/${month}/${year}.`);
-  };
+  date = new Date();
+  day = date.getDate();
+  month = date.getMonth() + 1;
+  year = date.getFullYear();
+  window.alert(`Today's date is ${day}/${month}/${year}.`);
+};
 
-  const promtDemo = () => {
-    let n = window.prompt("Enter a number...");
-    console.log("your number is " + n);
-    let fact = 1;
-    for (let i = n; i > 1; i--) {
-      fact *= i;
-    }
-    window.alert(" The factorial of " + n + " is " + fact + ".");
-  };
+const promptDemo = () => {
+  let num = window.prompt("Enter a number...");
+  console.log("Your number is " + num);
+  let fact = 1;
+  for (let i = num; i > 1; i--) {
+    fact *= i;
+  }
+  window.alert("The factorial of " + num + " is " + fact + ".");
+};
 
-  const alertDemo = () => {
-    let n = window.prompt("Enter a number...");
-    str = "";
-    for (let i = 1; i <= 10; i++) {
-      str += `${n} x ${i} = ${n * i} \n`;
-    }
-    window.alert("Multiplication table of " + parseInt(n) + "\n" + str);
-  };
+const alertDemo = () => {
+  let num = window.prompt("Enter a number...");
+  str = "";
+  for (let i = 1; i <= 10; i++) {
+    str += `${num} x ${i} = ${num * i} \n`;
+  }
+  window.alert("Multiplication table of " + parseInt(num) + "\n" + str);
+};
 
-  const confirmDemo = () => {
-    let num1 = window.prompt("Enter number 1");
-    if (
-      window.confirm(
-        "Press OK if number 2 = 10\nPress Cancel if number 2 = 20"
-      )
-    ) {
-      num2 = 10;
+const confirmDemo = () => {
+  let num1 = window.prompt("Enter number 1...");
+  if (
+    window.confirm("Press OK if number 2 = 10\nPress Cancel if number 2 = 20")
+  ) {
+    num2 = 10;
+  } else {
+    num2 = 20;
+  }
+  let num3 = window.prompt("Enter number 3...");
+  num1 = parseInt(num1);
+  num2 = parseInt(num2);
+  num3 = parseInt(num3);
+  window.alert(
+    num1 + " + " + num2 + " + " + num3 + " = " + (num1 + num2 + num3)
+  );
+};
+
+const convertRoman = () => {
+  let num = window.prompt("Enter a number...");
+  let og = num;
+  let i = 0;
+  var l = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var sym = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+  ];
+  var str = "";
+  while (num > 0) {
+    if (l[i] <= num) {
+      str += sym[i];
+      num -= l[i];
     } else {
-      num2 = 20;
+      i++;
     }
-    let num3 = window.prompt("Enter number 3");
-    num1 = parseInt(num1);
-    num2 = parseInt(num2);
-    num3 = parseInt(num3);
-    window.alert(
-      num1 + " + " + num2 + " + " + num3 + " = " + (num1 + num2 + num3)
-    );
-  };
+  }
+  window.alert("The number " + og + " in Roman Numerals is " + str + ".");
+};
